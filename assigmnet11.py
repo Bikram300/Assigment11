a20a4483957a0a1954093932a22f17f856fac2a0
Bikram300 <32117369+Bikram300@users.noreply.github.com>
2018-06-24 22:18:28 +0530

    Update assigmnet11.py
    change command

--- assigmnet11.py
+++ assigmnet11.py
@@ -29,8 +29,8 @@ t.start()
 
 
 
-'''Ques 3. Make a list that has 5 elements.Create a threading process that prints the 5 elements of the list with a delay of
-multiple of 2 sec between each display. Delay goes like 2sec-4sec-6sec-8sec-10sec.'''
+#Ques 3. Make a list that has 5 elements.Create a threading process that prints the 5 elements of the list with a delay of
+multiple of 2 sec between each display. Delay goes like 2sec-4sec-6sec-8sec-10sec.
 
 
 import threading
